e2da4cf0751d6a00dd2da66683f602f8be6ef6ea
LiDing <72634327+linkedlist771@users.noreply.github.com>
2023-11-29 17:12:31 +0800

    Site updated: 2023-11-29 17:12:28

--- change_image_path.py
+++ change_image_path.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 typro_image_dir_path = r"C:\Users\23174\AppData\Roaming\Typora\typora-user-images"
-hexo_image_dir_path = r"C:\Users\23174\Desktop\个人blog\blog\source\images"
+hexo_image_dir_path = r"C:\Users\23174\Desktop\blog\blog\source\images"
 
 # 遍历Typora图片目录中的所有文件
 for image_name in os.listdir(typro_image_dir_path):
@@ -10,13 +10,12 @@ for image_name in os.listdir(typro_image_dir_path):
     target_image_path = os.path.join(hexo_image_dir_path, image_name)
 
     # 检查目标目录中是否已经存在同名文件
-    if not os.path.exists(target_image_path):
-        # 如果不存在，则复制
-        try:
-            shutil.copy(source_image_path, target_image_path)
-            print(f"Copied {image_name} to Hexo image directory.")
-        except Exception as e:
-            print(f"Failed to copy {image_name} to Hexo image directory with error: {e}")
+    # 如果不存在，则复制
+    try:
+        shutil.copy(source_image_path, target_image_path)
+        print(f"Copied {image_name} to Hexo image directory.")
+    except Exception as e:
+        print(f"Failed to copy {image_name} to Hexo image directory with error: {e}")
     else:
         print(f"{image_name} already exists in Hexo image directory. Skipping.")
 
@@ -36,14 +35,28 @@ def replace_image_path_in_md_files(root_dir):
                     file_contents = file.read()
                     
                     # 替换图片路径
-                    new_contents = file_contents.replace(
-                        r'../../../../../AppData/Roaming/Typora/typora-user-images/',
-                        r'../images/'
-                    )
-                    new_contents = new_contents.replace(
-                        r'../../../../AppData/Roaming/Typora/typora-user-images/',
-                        r'../images/'
-                    )
+                    pattern1 = r'../../../../../AppData/Roaming/Typora/typora-user-images/'
+                    pattern2 = r'../../../../AppData/Roaming/Typora/typora-user-images/'
+                    pattern3 = r'C:/Users/23174/AppData/Roaming/Typora/typora-user-images/'
+                    if pattern1 in file_contents:
+                        new_contents = file_contents.replace(
+                            pattern1,
+                            r'../images/'
+                        )
+                    elif pattern2 in file_contents:
+                        new_contents = new_contents.replace(
+                            pattern2,
+                            r'../images/'
+
+                        )
+                    elif pattern3 in file_contents:
+                        new_contents = new_contents.replace(
+                            pattern3,
+                            r'../images/'
+                        )
+                    else:
+                        new_contents = ""
+
                 
                 # 如果内容有变化，写回文件
                 if new_contents != file_contents:
@@ -52,4 +65,8 @@ def replace_image_path_in_md_files(root_dir):
                     print(f"Updated image paths in {file_path}")
 
 # 调用函数，传入你想开始搜索的根目录（这里是当前目录）
+
 replace_image_path_in_md_files(os.getcwd())
+
+# wait for user input to exit
+input("Press Enter to exit.")
